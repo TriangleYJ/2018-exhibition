1fa33882de5e776ac836fbfb207d52d1434fff69
triangleyj <dpwns99@gmail.com>
2018-02-16 18:26:27 +0900

    한글화 및 PEP8 스타일 가이드 적용

--- fl-dqn/deep_q_network.py
+++ fl-dqn/deep_q_network.py
@@ -4,39 +4,45 @@ from __future__ import print_function
 import tensorflow as tf
 import cv2
 import sys
-sys.path.append("game/")
 import wrapped_flappy_bird as game
 import random
 import numpy as np
 from collections import deque
 
-GAME = 'bird' # the name of the game being played for log files
-ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 10000. # timesteps to observe before training
-EXPLORE = 3000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.1 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
-BATCH = 32 # size of minibatch
+sys.path.append("game/")
+
+GAME = 'bird'  # 로그 파일을 위한 재생되어지고 있던 게임
+ACTIONS = 2  # 유효한 액션 의 수
+GAMMA = 0.99  # 이전 관찰들로부터의 감소율
+OBSERVE = 10000.  # 훈련 전에 관찰하기 위한 타임 스텝
+EXPLORE = 3000000.  # 입실론 값을 강화하기 위한 최소 프레임수
+FINAL_EPSILON = 0.0001  # 입실론의 최종값
+INITIAL_EPSILON = 0.1  # 입실론 값의 시작값
+REPLAY_MEMORY = 50000  # 기억해야 할 이전 변화의 수
+BATCH = 32  # 미니배치의 크기
 FRAME_PER_ACTION = 1
 
+
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
+    initial = tf.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
+
 
 def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
+
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="SAME")
+
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-def createNetwork():
-    # network weights
+
+def create_network():
+    # 네트워크 가중치
     W_conv1 = weight_variable([8, 8, 4, 32])
     b_conv1 = bias_variable([32])
 
@@ -52,56 +58,50 @@ def createNetwork():
     W_fc2 = weight_variable([512, ACTIONS])
     b_fc2 = bias_variable([ACTIONS])
 
-    # input layer
+    # 입력 층
     s = tf.placeholder("float", [None, 80, 80, 4])
 
-    # hidden layers
+    # 숨겨진 층들
     h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
 
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
 
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
-    # readout layer
+    # 판독 값 층
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
     return s, readout, h_fc1
 
-def trainNetwork(s, readout, h_fc1, sess):
-    # define the cost function
+
+def train_network(s, readout, sess):
+    # 비용 함수를 정의한다
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
     readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
     cost = tf.reduce_mean(tf.square(y - readout_action))
     train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
-    # open up a game state to communicate with emulator
+    # 에뮬레이터와 통신하기 위해 게임 스테이트를 초기화한다
     game_state = game.GameState()
 
-    # store the previous observations in replay memory
+    # 리플레이 메모리에 이전 관찰들을 저장한다
     D = deque()
 
-    # printing
-    a_file = open("logs_" + GAME + "/readout.txt", 'w')
-    h_file = open("logs_" + GAME + "/hidden.txt", 'w')
-
-    # get the first state by doing nothing and preprocess the image to 80x80x4
+    # 아무것도 안함으로써 첫 상태를 가져오고 이미지를 80*80*4크기로 사전 작업을 한다
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
     x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
-    # saving and loading networks
+    # 네트워크를 저장/불러온다
     saver = tf.train.Saver()
     sess.run(tf.initialize_all_variables())
     '''
@@ -112,91 +112,87 @@ def trainNetwork(s, readout, h_fc1, sess):
     else:
         print("Could not find old network weights")'''
 
-    # start training
+    # 훈련 시작
     epsilon = INITIAL_EPSILON
     t = 0
-    while "flappy bird" != "angry bird":
-        # choose an action epsilon greedily
-        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
+    while True:
+        # 욕심내어 액션 입실론을 선택한다
+        readout_t = readout.eval(feed_dict={s: [s_t]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
-                print("----------Random Action----------")
+                print("----------랜덤 액션----------")
                 action_index = random.randrange(ACTIONS)
                 a_t[random.randrange(ACTIONS)] = 1
             else:
                 action_index = np.argmax(readout_t)
                 a_t[action_index] = 1
         else:
-            a_t[0] = 1 # do nothing
+            a_t[0] = 1  # 아무것도 안한다
 
-        # scale down epsilon
+        # 입실론 크기를 줄인다
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
-        # run the selected action and observe next state and reward
+        # 선택된 액션을 실행하고 다음 상태와 보상을 관찰한다
         x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
         s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
-        # store the transition in D
+        # D에 변화를 저장한다
         D.append((s_t, a_t, r_t, s_t1, terminal))
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
-        # only train if done observing
+        # 관찰이 끝날 시에만 훈련한다
         if t > OBSERVE:
-            # sample a minibatch to train on
+            # 훈련을 진행할 미니배치의 견본을 뽑는다
             minibatch = random.sample(D, BATCH)
 
-            # get the batch variables
+            # 배치 변수들을 가져온다
             s_j_batch = [d[0] for d in minibatch]
             a_batch = [d[1] for d in minibatch]
             r_batch = [d[2] for d in minibatch]
             s_j1_batch = [d[3] for d in minibatch]
 
             y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
+            readout_j1_batch = readout.eval(feed_dict={s: s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
-                # if terminal, only equals reward
+                # 터미널인 경우 보상과 같다
                 if terminal:
                     y_batch.append(r_batch[i])
                 else:
                     y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
 
-            # perform gradient step
-            train_step.run(feed_dict = {
-                y : y_batch,
-                a : a_batch,
-                s : s_j_batch}
+            # 점진적 하강 단계를 수행한다
+            train_step.run(feed_dict={
+                y: y_batch,
+                a: a_batch,
+                s: s_j_batch}
             )
 
-        # update the old values
+        # 오래된 값들을 업데이트한다
         s_t = s_t1
         t += 1
 
-        # save progress every 10000 iterations
+        # 매 10000번마다 경과를 저장한다
         if t % 10000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
 
-        # print info
-        state = ""
+        # 출력 정보
         if t <= OBSERVE:
-            state = "observe"
-        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
-            state = "explore"
+            state = "관찰"
+        elif OBSERVE < t <= OBSERVE + EXPLORE:
+            state = "탐구"
         else:
-            state = "train"
+            state = "훈련"
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-            "/ Q_MAX %e" % np.max(readout_t))
-        # write info to files
+        print("타임스텝", t, "/ 상태", state, "/ 입실론", epsilon, "/ 액션", action_index, "/ 보상", r_t, "/ Q 최대값 %e" % np.max(readout_t))
+        # 파일에 저장
         '''
         if t % 10000 <= 100:
             a_file.write(",".join([str(x) for x in readout_t]) + '\n')
@@ -204,13 +200,16 @@ def trainNetwork(s, readout, h_fc1, sess):
             cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
         '''
 
-def playGame():
+
+def play_game():
     sess = tf.InteractiveSession()
-    s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, h_fc1, sess)
+    s, readout, h_fc1 = create_network()
+    train_network(s, readout, sess)
+
 
 def main():
-    playGame()
+    play_game()
+
 
 if __name__ == "__main__":
     main()
